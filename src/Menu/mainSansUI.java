2fd755f73c4a803de3e74fd02fcec2077e29122a
BelharratMohamed <belharratmohamed@gmail.com>
2025-12-20 18:48:55 +0100

    changement du nom de la classe

--- src/Menu/mainSansUI.java
+++ src/Menu/mainSansUI.java
@@ -7,7 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class mainSansUI {
+public class MainSansUI {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         Reseau r = new Reseau(10);
