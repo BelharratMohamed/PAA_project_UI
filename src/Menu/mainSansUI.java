cf3093bac31eadf6c64afa309c6facb9fa1cf7e4
BelharratMohamed <belharratmohamed@gmail.com>
2025-12-20 18:48:29 +0100

    test

--- src/Menu/mainSansUI.java
+++ src/Menu/mainSansUI.java
@@ -7,7 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class MainSansUI {
+public class mainSansUI {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         Reseau r = new Reseau(10);
